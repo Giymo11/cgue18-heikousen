727f9c071c796a32b07a7dd483c7a9a360625496
Giymo11 <gizmo.head@yahoo.de>
2018-03-09 11:45:09 +0100

    WIP adds glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <vulkan/vulkan.h>
 #include <vector>
+
+// #include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include "debug_trap.h"
 
@@ -9,15 +12,172 @@ if(val != VK_SUCCESS)\
     psnip_trap();\
 
 
-int initVulkan();
-
 VkInstance instance;
 VkDevice device;
+
+GLFWwindow *window;
+
+
+void printStats(VkPhysicalDevice &device);
+
+void startGlfw() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(1200, 720, "heikousen", nullptr, nullptr);
+}
+
+void startVulkan() {
+    VkResult result;
+
+
+    VkApplicationInfo applicationInfo = {};
+    // describes what kind of struct this is, because otherwise the type info is lost when passed to the driver
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    // used for extensions
+    applicationInfo.pNext = nullptr;
+    applicationInfo.pApplicationName = "heikousen";
+    applicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    applicationInfo.pEngineName = "JoJo Engine";
+    applicationInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    applicationInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+    uint32_t numberOfLayers = 0;
+    vkEnumerateInstanceLayerProperties(&numberOfLayers, nullptr);
+    auto *layers = new VkLayerProperties[numberOfLayers];
+    vkEnumerateInstanceLayerProperties(&numberOfLayers, layers);
+
+    std::cout << std::endl << "Amount of instance layers: " << numberOfLayers << std::endl << std::endl;
+    for (int i = 0; i < numberOfLayers; ++i) {
+        std::cout << "Name:            " << layers[i].layerName << std::endl;
+        std::cout << "Spec Version:    " << layers[i].specVersion << std::endl;
+        std::cout << "Description:     " << layers[i].description << std::endl << std::endl;
+    }
+
+
+    uint32_t numberOfExtensions = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &numberOfExtensions, nullptr);
+    auto *extensions = new VkExtensionProperties[numberOfExtensions];
+    vkEnumerateInstanceExtensionProperties(nullptr, &numberOfExtensions, extensions);
+
+    std::cout << std::endl << "Amount of instance extensions: " << numberOfExtensions << std::endl << std::endl;
+    for (int i = 0; i < numberOfExtensions; ++i) {
+        std::cout << "Name:            " << extensions[i].extensionName << std::endl;
+        std::cout << "Spec Version:    " << extensions[i].specVersion << std::endl << std::endl;
+    }
+
+    const std::vector<const char*> usedValidationLayers = {
+            "VK_LAYER_LUNARG_standard_validation"
+    };
+
+
+
+    VkInstanceCreateInfo instanceCreateInfo = {};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pNext = nullptr;
+    // Flags are reserved for future use
+    instanceCreateInfo.flags = 0;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+    // Layers are used for debugging, profiling, error handling,
+    instanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(usedValidationLayers.size());
+    instanceCreateInfo.ppEnabledLayerNames = usedValidationLayers.data();
+    // Extensions are used to extend vulkan functionality
+    instanceCreateInfo.enabledExtensionCount = 0;
+    instanceCreateInfo.ppEnabledExtensionNames = nullptr;
+
+
+    // vulkan instance is similar to OpenGL context
+    // by telling vulkan which extensions we plan on using, it can disregard all others -> performance gain in comparison to openGL
+    result = vkCreateInstance(&instanceCreateInfo, nullptr, &instance);
+    ASSERT_VULKAN(result);
+
+
+    uint32_t numberOfPhysicalDevices = 0;
+
+    // if passed nullptr as third parameter, outputs the number of GPUs to the second parameter
+    result = vkEnumeratePhysicalDevices(instance, &numberOfPhysicalDevices, nullptr);
+    ASSERT_VULKAN(result);
+
+    auto *physicalDevices = new VkPhysicalDevice[numberOfPhysicalDevices];
+    // actually enumerates the GPUs for use
+    result = vkEnumeratePhysicalDevices(instance, &numberOfPhysicalDevices, physicalDevices);
+    ASSERT_VULKAN(result);
+
+    std::cout << std::endl << "GPUs Found: " << numberOfPhysicalDevices << std::endl << std::endl;
+
+    for (int i = 0; i < numberOfPhysicalDevices; ++i) {
+        printStats(physicalDevices[i]);
+    }
+
+
+    float queuePriorities[]{1.0f};
+
+    VkDeviceQueueCreateInfo deviceQueueCreateInfo;
+    deviceQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    deviceQueueCreateInfo.pNext = nullptr;
+    deviceQueueCreateInfo.flags = 0;
+    deviceQueueCreateInfo.queueFamilyIndex = 0; // TODO: choose the best queue family
+    deviceQueueCreateInfo.queueCount = 1; // TODO: check how many families are supported, 4 would be better
+    deviceQueueCreateInfo.pQueuePriorities = queuePriorities;
+
+    VkPhysicalDeviceFeatures usedFeatures = {};
+
+    VkDeviceCreateInfo deviceCreateInfo;
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    deviceCreateInfo.pNext = nullptr;
+    deviceCreateInfo.flags = 0;
+    deviceCreateInfo.queueCreateInfoCount = 1;
+    deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo;
+    deviceCreateInfo.enabledLayerCount = 0;
+    deviceCreateInfo.ppEnabledLayerNames = nullptr;
+    deviceCreateInfo.enabledExtensionCount = 0;
+    deviceCreateInfo.ppEnabledExtensionNames = nullptr;
+    deviceCreateInfo.pEnabledFeatures = &usedFeatures;
+
+
+    result = vkCreateDevice(physicalDevices[0], &deviceCreateInfo, nullptr, &device); // TODO: choose right physical device
+    ASSERT_VULKAN(result)
+
+    VkQueue queue;
+    vkGetDeviceQueue(device, 0, 0, &queue);
+
+
+
+    delete[] layers;
+    delete[] extensions;
+    delete[] physicalDevices;
+}
+
+void gameloop() {
+    while(true) {
+
+    }
+}
+
+void shutdownVulkan() {
+    // block until vulkan has finished
+    vkDeviceWaitIdle(device);
+
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(instance, nullptr);
+}
+
+void shutdownGlfw() {
+
+}
 
 int main() {
     std::cout << "Hello World!" << std::endl << std::endl;
 
-    initVulkan();
+    startGlfw();
+    startVulkan();
+
+    gameloop();
+
+    shutdownVulkan();
+    shutdownGlfw();
 
     return 0;
 }
@@ -67,7 +227,7 @@ void printStats(VkPhysicalDevice &device) {
 
 
     uint32_t numberOfQueueFamilies = 0;
-    vkGetPhysicalDeviceQueueFamilyProperties(device, &numberOfQueueFamilies, NULL);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &numberOfQueueFamilies, nullptr);
 
     auto *queueFamilyProperties = new VkQueueFamilyProperties[numberOfQueueFamilies];
     vkGetPhysicalDeviceQueueFamilyProperties(device, &numberOfQueueFamilies, queueFamilyProperties);
@@ -86,129 +246,4 @@ void printStats(VkPhysicalDevice &device) {
 
     std::cout << std::endl;
     delete[] queueFamilyProperties;
-}
-
-int initVulkan() {
-    VkResult result;
-
-
-    VkApplicationInfo applicationInfo = {};
-    // describes what kind of struct this is, because otherwise the type info is lost when passed to the driver
-    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    // used for extensions
-    applicationInfo.pNext = NULL;
-    applicationInfo.pApplicationName = "heikousen";
-    applicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    applicationInfo.pEngineName = "JoJo Engine";
-    applicationInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    applicationInfo.apiVersion = VK_API_VERSION_1_0;
-
-
-    uint32_t numberOfLayers = 0;
-    vkEnumerateInstanceLayerProperties(&numberOfLayers, NULL);
-    auto *layers = new VkLayerProperties[numberOfLayers];
-    vkEnumerateInstanceLayerProperties(&numberOfLayers, layers);
-
-    std::cout << std::endl << "Amount of instance layers: " << numberOfLayers << std::endl << std::endl;
-    for (int i = 0; i < numberOfLayers; ++i) {
-        std::cout << "Name:            " << layers[i].layerName << std::endl;
-        std::cout << "Spec Version:    " << layers[i].specVersion << std::endl;
-        std::cout << "Description:     " << layers[i].description << std::endl << std::endl;
-    }
-
-
-    uint32_t numberOfExtensions = 0;
-    vkEnumerateInstanceExtensionProperties(NULL, &numberOfExtensions, NULL);
-    auto *extensions = new VkExtensionProperties[numberOfExtensions];
-    vkEnumerateInstanceExtensionProperties(NULL, &numberOfExtensions, extensions);
-
-    std::cout << std::endl << "Amount of instance extensions: " << numberOfExtensions << std::endl << std::endl;
-    for (int i = 0; i < numberOfExtensions; ++i) {
-        std::cout << "Name:            " << extensions[i].extensionName << std::endl;
-        std::cout << "Spec Version:    " << extensions[i].specVersion << std::endl << std::endl;
-    }
-
-    const std::vector<const char *> usedValidationLayers = {
-            "VK_LAYER_LUNARG_standard_validation"
-    };
-
-
-    VkInstanceCreateInfo instanceCreateInfo = {};
-    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instanceCreateInfo.pNext = NULL;
-    // Flags are reserved for future use
-    instanceCreateInfo.flags = 0;
-    instanceCreateInfo.pApplicationInfo = &applicationInfo;
-    // Layers are used for debugging, profiling, error handling,
-    instanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(usedValidationLayers.size());
-    instanceCreateInfo.ppEnabledLayerNames = usedValidationLayers.data();
-    // Extensions are used to extend vulkan functionality
-    instanceCreateInfo.enabledExtensionCount = 0;
-    instanceCreateInfo.ppEnabledExtensionNames = NULL;
-
-
-    // vulkan instance is similar to OpenGL context
-    // by telling vulkan which extensions we plan on using, it can disregard all others -> performance gain in comparison to openGL
-    result = vkCreateInstance(&instanceCreateInfo, NULL, &instance);
-    ASSERT_VULKAN(result);
-
-
-    uint32_t numberOfPhysicalDevices = 0;
-
-    // if passed NULL as third parameter, outputs the number of GPUs to the second parameter
-    result = vkEnumeratePhysicalDevices(instance, &numberOfPhysicalDevices, NULL);
-    ASSERT_VULKAN(result);
-
-    auto *physicalDevices = new VkPhysicalDevice[numberOfPhysicalDevices];
-    // actually enumerates the GPUs for use
-    result = vkEnumeratePhysicalDevices(instance, &numberOfPhysicalDevices, physicalDevices);
-    ASSERT_VULKAN(result);
-
-    std::cout << std::endl << "GPUs Found: " << numberOfPhysicalDevices << std::endl << std::endl;
-
-    for (int i = 0; i < numberOfPhysicalDevices; ++i) {
-        printStats(physicalDevices[i]);
-    }
-
-
-    float queuePriorities[]{1.0f};
-
-    VkDeviceQueueCreateInfo deviceQueueCreateInfo;
-    deviceQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-    deviceQueueCreateInfo.pNext = NULL;
-    deviceQueueCreateInfo.flags = 0;
-    deviceQueueCreateInfo.queueFamilyIndex = 0; // TODO: choose the best queue family
-    deviceQueueCreateInfo.queueCount = 1; // TODO: check how many families are supported, 4 would be better
-    deviceQueueCreateInfo.pQueuePriorities = queuePriorities;
-
-    VkPhysicalDeviceFeatures usedFeatures = {};
-
-    VkDeviceCreateInfo deviceCreateInfo;
-    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    deviceCreateInfo.pNext = NULL;
-    deviceCreateInfo.flags = 0;
-    deviceCreateInfo.queueCreateInfoCount = 1;
-    deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo;
-    deviceCreateInfo.enabledLayerCount = 0;
-    deviceCreateInfo.ppEnabledLayerNames = NULL;
-    deviceCreateInfo.enabledExtensionCount = 0;
-    deviceCreateInfo.ppEnabledExtensionNames = NULL;
-    deviceCreateInfo.pEnabledFeatures = &usedFeatures;
-
-
-    result = vkCreateDevice(physicalDevices[0], &deviceCreateInfo, NULL, &device); // TODO: choose right physical device
-    ASSERT_VULKAN(result)
-
-
-    // block until vulkan has finished
-    vkDeviceWaitIdle(device);
-
-    vkDestroyDevice(device, NULL);
-    vkDestroyInstance(instance, NULL);
-
-    delete[] layers;
-    delete[] extensions;
-    delete[] physicalDevices;
-
-    return 0;
 }
