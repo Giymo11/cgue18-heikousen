d1b735898e83e2a26b5f5eedb6685cfc7bb65297
Michael Pucher <michael.pucher.main@gmail.com>
2018-03-13 13:44:55 +0100

    Adding (working?) V8 build script and example

--- build_v8.bat
+++ build_v8.bat
@@ -6,22 +6,26 @@ set GYP_MSVS_VERSION=2017
 
 cmd /c gclient
 
-rem Sync
+rem Fetch
 cd "%~dp0extern"
 cmd /c fetch v8
+
+rem Pin version and sync
 cd "%~dp0extern\v8"
+cmd /c git checkout 6.7.43
 cmd /c gclient sync
 
 rem Generate
-cmd /c git checkout 6.7.41
 cmd /c python tools/dev/v8gen.py x64.release
 
 rem Fix args.gn
-echo is_component_build = false >>  out.gn\x64.release\args.gn
-echo v8_static_library = true >>  out.gn\x64.release\args.gn
+echo v8_static_library = true >> out.gn\x64.release\args.gn
+echo v8_use_external_startup_data = false >> out.gn\x64.release\args.gn 
+cmd /c gn gen x64.release
 
-rem Compile
+rem rem Compile
 cmd /c ninja -C out.gn/x64.release
+cd "%~dp0"
 
 rem Copy generated libs
 cd "%~dp0extern\v8\out.gn\x64.release\obj"
@@ -29,5 +33,5 @@ for /r %%a in (*.lib) do xcopy "%%a" "%~dp0extern\dist\lib" /Y /i
 cd "%~dp0"
 
 rem Copy headers
-mkdir "%~dp0extern\dist\include"
-copy "%~dp0extern\v8\include" "%~dp0extern\dist\include\v8"
+mkdir "%~dp0extern\dist\include\v8"
+xcopy /s/Y "%~dp0extern\v8\include" "%~dp0extern\dist\include\v8"
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <v8/libplatform/libplatform.h>
+#include <v8/v8.h>
+#include <memory>
+
 #include "jojo_vulkan_data.hpp"
 #include "jojo_vulkan.hpp"
 #include "jojo_vulkan_utils.hpp"
@@ -493,9 +497,55 @@ void shutdownGlfw() {
     glfwTerminate();
 }
 
+void hello_v8(const char *data_dir) {
+    // Initialize V8.
+    v8::V8::InitializeICUDefaultLocation(data_dir);
+    v8::V8::InitializeExternalStartupData(data_dir);
+    std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+    v8::V8::InitializePlatform(platform.get());
+    v8::V8::Initialize();
 
-int main() {
-    std::cout << "Hello World!" << std::endl << std::endl;
+    // Create a new Isolate and make it the current one.
+    v8::Isolate::CreateParams create_params;
+    create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    v8::Isolate* isolate = v8::Isolate::New(create_params);
+    {
+        v8::Isolate::Scope isolate_scope(isolate);
+
+        // Create a stack-allocated handle scope.
+        v8::HandleScope handle_scope(isolate);
+
+        // Create a new context.
+        v8::Local<v8::Context> context = v8::Context::New(isolate);
+
+        // Enter the context for compiling and running the hello world script.
+        v8::Context::Scope context_scope(context);
+
+        // Create a string containing the JavaScript source code.
+        v8::Local<v8::String> source = v8::String::NewFromUtf8(isolate, "'Hello' + ', World!'", v8::NewStringType::kNormal)
+            .ToLocalChecked();
+
+        // Compile the source code.
+        v8::Local<v8::Script> script = v8::Script::Compile(context, source).ToLocalChecked();
+
+        // Run the script to get the result.
+        v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+        // Convert the result to an UTF8 string and print it.
+        v8::String::Utf8Value utf8(isolate, result);
+        std::cout << *utf8;
+    }
+
+    // Dispose the isolate and tear down V8.
+    isolate->Dispose();
+    v8::V8::Dispose();
+    v8::V8::ShutdownPlatform();
+    delete create_params.array_buffer_allocator;
+}
+
+
+int main(int argc, char *argv[]) {
+    hello_v8(argv[0]);
 
     startGlfw();
     startVulkan();
